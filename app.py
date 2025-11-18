edd2bfeff164fcc2caa9c9e7301a3173ebbea0ae
sindhuragajjala <121331979+sindhuragajjala@users.noreply.github.com>
2025-11-18 09:20:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,9 +41,10 @@ st.write(type((J)))'''
 # Make prediction
 if st.button("Predict"):
     # Assuming your model expects a DataFrame or specific input format
-    input_data = pd.DataFrame([['A','B','C','D','E','F','G','H','I','J']], columns=['scaled','Study hours','Attendance','health','Internet Access','Region','D/H','Time',"Parent's educated",'class response'])
-    prediction = model.predict([input_data])
+    input_data = pd.DataFrame([[A,B,C,D,E,F,G,H,I,J]], columns=['scaled','Study hours','Attendance','health','Internet Access','Region','D/H','Time',"Parent's educated",'class response'])
+    prediction = model.predict(input_data)
     st.write(f"The prediction is: {prediction[0]}")
+
 
 
 
