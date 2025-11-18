4fc0ed9ddabbf017e8771efd81e4bde2b91384d8
sindhuragajjala <121331979+sindhuragajjala@users.noreply.github.com>
2025-11-18 12:46:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,19 +24,6 @@ H=st.slider("Time",5,40,1)
 I=st.slider("Parent's educated",0,1,1)
 J=st.slider("class response",0,2,1)
 
-st.write(type((A)))
-'''
-st.write(type((B)))
-st.write(type((C)))
-st.write(type((D)))
-st.write(type((E)))
-st.write(type((F)))
-st.write(type((G)))
-st.write(type((H)))
-st.write(type((I)))
-st.write(type((J)))'''
-# Preprocess input (if necessary)
-# ...
 
 # Make prediction
 if st.button("Predict"):
@@ -44,6 +31,7 @@ if st.button("Predict"):
     input_data = pd.DataFrame([[A,B,C,D,E,F,G,H,I,J]], columns=['scaled','Study hours','Attendance','health','Internet Access','Region','D/H','Time',"Parent's educated",'class response'])
     prediction = model.predict(input_data)
     st.write(f"The prediction is: {prediction[0]}")
+
 
 
 
